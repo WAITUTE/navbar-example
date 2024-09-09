873893c56c73c17d4b8ea505b5dc30a1a454d42e
WAITUTE <109899422+WAITUTE@users.noreply.github.com>
2024-09-09 17:12:10 +0300

    Add files via upload

---
+++ src/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './home.css';
+import Navbar from '../components/navbar/Navbar';
+import  { useState } from "react";
+import { Layout } from "antd";
+
+const Home = () => {
+    
+    return (
+    <Navbar  />  )
+}
+
+export default Home
